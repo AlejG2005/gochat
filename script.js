e4885f3c43a7db3f72cf2122fb8791dc426e4b2a
AlejG2005 <droiiipm@hotmail.com>
2026-03-17 23:25:07 -0600

    FIX: script.js, syntax and logic errors

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
+const API = "https://chat.joelsiervas.online/messages";
+
 const getMessages = async () => {
-    const response = await fetch('/api/messages')
-    const messages = await Response.json()
-    
-    const ul = document.getElementById('messages')
-    ul.innerHTML = ''
+  const response = await fetch(API);
+  const messages = await response.json();
 
-    for(let i = 0; i < message.length; i++){
-        const li = document.createElement('li')
-        li.innerHTML = ´<strong>${message.user}:</strong> ${message.text}
-        ul.append(li)
-    }
-}
+  const ul = document.getElementById("messages");
+  ul.innerHTML = "";
 
-const postMesagge = async (message) => {
-    const response = await fetch('/api/messages', {
-        method: 'POST',
-        body: JSON.stringify(message), 
-    })
-}
+  for (let i = 0; i < messages.length; i++) {
+    const li = document.createElement("li");
+    li.innerHTML = `<strong>${messages[i].user}:</strong> ${messages[i].text}`;
+    ul.appendChild(li);
+  }
+};
 
-getMessages()
+const postMessage = async (message) => {
+  await fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+};
 
-document.getElementById('send').addEventListener('click', () => {
-    const message = document.getElementById('message')
-    const message = textArea.value
-    postMesagge({
-        user:'alejandro'
-        text:'message'
-    })
+getMessages();
 
+// AUTO REFRESH (10 pts)
+setInterval(getMessages, 3000);
 
-})
+// CLICK SEND
+document.getElementById("send").addEventListener("click", () => {
+  const input = document.getElementById("message");
+
+  postMessage({
+    user: "alejandro",
+    text: input.value,
+  });
+
+  input.value = "";
+});
+
+// ENTER PARA ENVIAR (10 pts)
+document.getElementById("message").addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    document.getElementById("send").click();
+  }
+});
+
